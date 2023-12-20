3719cdc6c4603f5b4b010a1324543c6f9d8a9246
Pravinsavaliya <149356568+Pravinsavaliya@users.noreply.github.com>
2023-12-20 10:12:37 +0530

    Add files via upload

---
+++ loop/lab work - lec2/Lab Work do while - Lec2/sum.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+
+    int i=1,n,sum=0;
+    printf("\nEnter on.:");
+    scanf("%d",&n);
+
+    do{
+        sum+=i;
+        i++;
+    }
+    while(i<=n);
+      printf("\n Sum is %d",sum);
+
+    return 0;
+}
+
---
+++ loop/lab work - lec2/Lab Work for loop- Lec2/1to10.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+
+    int i=1;
+
+    for(;i<=10;i++){
+        printf("\n %d",i);
+        }
+    return 0;
+}
