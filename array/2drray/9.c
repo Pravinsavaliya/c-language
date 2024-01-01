f5d9fdedc57260be5886d011fc3f2631787fb391
Pravinsavaliya <pravinsavaliya801@gmail.com>
2024-01-01 23:26:19 +0530

    array

--- array/2drray/9.c
+++ array/2drray/9.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main(){
-    int a[20][20],m,n,i,j,anti_diag=0,t;
+    int a[20][20],m,n,i,j,cross_diag=0,t;
     printf("Enter row and column:");
     scanf("%d %d",&m,&n);
-    if(m==n){
+if(m==n){
     printf("Eneter the array of elements:");
     for(i=0;i<m;i++){
         for(j=0;j<n;j++){
@@ -16,12 +16,12 @@ int main(){
         for(j=0;j<n;j++){
             printf("%d ",a[i][j]);
             if(i==j || i+j==t ){
-                anti_diag = anti_diag+a[i][j];
+                cross_diag = cross_diag+a[i][j];
             }
         }
         printf("\n");
     }
-    printf("\n diagonal sum of array element:%d",anti_diag);
+    printf("\n cross_diag sum of array element:%d",cross_diag);
     } else{
         printf("\n Eneter sam row and column...");
     }
