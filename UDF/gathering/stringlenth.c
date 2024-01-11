66c5471f80358121f47091b0b2dfcb302e9c61d4
Pravinsavaliya <pravinsavaliya801@gmail.com>
2024-01-11 12:14:19 +0530

    udf

---
+++ UDF/gathering/stringlenth.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int lenth(char arr[30]){
+    int i,count=0;
+    for(i=0;arr[i]!="\0";i++){
+        count++;
+    }
+}
+void main(){
+    int
+}
---
+++ UDF/labwork2/cal.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+float add(float a, float b);
+float subtract(float a, float b);
+float multiply(float a, float b);
+float divide(float a, float b);
+
+int main() {
+    float num1, num2, result;
+    char op;
+
+    while (1) { 
+        printf("Enter  number1 and number2: ");
+        scanf("%f %f", &num1,&num2);
+        printf("Enter operator (+, -, *, /): ");
+        scanf(" %c", &op);
+
+        switch (op) {
+            case '+':
+                result = add(num1, num2);
+                break;
+            case '-':
+                result = subtract(num1, num2);
+                break;
+            case '*':
+                result = multiply(num1, num2);
+                break;
+            case '/':
+                result = divide(num1, num2);
+                break;
+            default:
+                printf("Invalid operator\n");
+                continue; 
+        }
+        printf("Result: %.2f\n\n", result);
+    }
+
+    return 0;
+}
+
+float add(float a, float b) {
+    return a + b;
+}
+
+float subtract(float a, float b) {
+    return a - b;
+}
+
+float multiply(float a, float b) {
+    return a * b;
+}
+
+float divide(float a, float b) {
+    return a / b;
+}
